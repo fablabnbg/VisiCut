770f840e77a3f9cada237e44065c28004a415961
Juergen Weigert <jnweiger@gmail.com>
2021-09-20 16:36:08 +0200

    made s.send compatible with both python2 and python3

--- distribute/distribute_jw2018+1.sh
+++ distribute/distribute_jw2018+1.sh
@@ -10,7 +10,9 @@ cd "$(dirname "$0")/.."
 if ! grep 'VERSION="18.04' /etc/os-release ; then
   echo "Must run 18.04";
   echo "Try:"
-  echo " docker run --rm -ti -v$(pwd -P):/mnt/VisiCut ubuntu:18.04 bash"
+  echo " docker build -t visibuild ."
+  echo " docker run --rm -ti -u$(id -u):$(id -g) -v$(pwd -P):/v visibuild bash /v/distribute/$(basename $0)"
+  echo ""
   exit 1
 fi
 
@@ -21,12 +23,32 @@ if [ -f $app_prop.orig ]; then
 fi
 
 ubuntu_builddeps="librsvg2-bin dpkg fakeroot checkinstall ant nsis zip"
-dpkg -l $ubuntu_builddeps > /dev/null || sudo apt install $ubuntu_builddeps
+dpkg -l $ubuntu_builddeps > /dev/null || sudo apt install $ubuntu_builddeps || apt install $ubuntu_builddeps
 
 VERSION=$(sed -ne 's/Application.version\s*=\s*//p' $app_prop)
 VERSION=1.8-310.1+20181009jw+1
-
 # VERSION="$(git describe --tags)+dev$(date +%Y%m%d)jw"
+
+cleanup()
+{
+  trap - EXIT  # do not execut twice
+  cd "$(dirname "$0")/.."
+  if [ -f "$app_prop.orig" ]; then
+    echo "... restoring VisicutApp.properties ... (but saving the patch)"
+    mv -f $app_prop $app_prop.jw
+    mv $app_prop.orig $app_prop
+    diff -u0 $app_prop $app_prop.jw || true
+  fi
+  if [ -d "$ext_dir.orig" ]; then
+    rm -rf $ext_dir.2018
+    mv $ext_dir $ext_dir.2018
+    mv $ext_dir.orig $ext_dir
+    md5sum $ext_dir.2018/*
+  fi
+}
+
+trap cleanup EXIT HUP INT TERM
+
 sed -i.orig -e "s@Application.version.*@Application.version = $VERSION@" $app_prop
 echo "... temporarily patched $app_prop to say"
 grep Application.version $app_prop
@@ -47,27 +69,11 @@ if [ ! -d $ext_dir.orig ]; then
   for file in visicut_export.inx visicut_export_replace.inx daemonize.py visicut_export.py; do
     $wget $dl_e/$file -O $file
   done
+  sed -i -e 's@s.send(\(.*\)@s.send(str(\1.encode("utf-8"))@' visicut_export.py		# make s.send compatible with both python2 and 3
+  sed -i -e 's/^if IMPORT:/if IMPORT and False:  # --add is not implemented in old 1.8.x/' visicut_export.py
+  sed -i -e 's/>Add to VisiCut</>Add, if VisiCut is running</' visicut_export.inx	# discourage this option
 fi
 cd "$(dirname "$0")"
-
-
-cleanup()
-{
-  trap - EXIT  # do not execut twice
-  cd "$(dirname "$0")/.."
-  if [ -f "$app_prop.orig" ]; then
-    echo "... restoring VisicutApp.properties ... (but saving the patch)"
-    mv -f $app_prop $app_prop.jw
-    mv $app_prop.orig $app_prop
-    diff -u0 $app_prop $app_prop.jw || true
-  fi
-  if [ -d "$ext_dir.orig" ]; then
-    rm -rf $ext_dir
-    mv $ext_dir.orig $ext_dir
-  fi
-}
-
-trap cleanup EXIT HUP INT TERM
 
 if [ -f /usr/bin/visicut ]; then
   echo "Uninstalling visicut"
@@ -75,3 +81,4 @@ if [ -f /usr/bin/visicut ]; then
   sudo apt-get purge visicut
 fi
 bash ./distribute.sh "$@"
+ls -l *$VERSION*
