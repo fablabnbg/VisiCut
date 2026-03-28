486aecdee3abbdfeee4559931303b3c36c48e407
Tom Tchilov <tom@tchilov.de>
2026-03-28 22:19:29 +0100

    Revert "generatesplash: move from rsvg-convert to magick"
    This reverts commit bf794a722762ba5b5139a94c5782155e6dbae3b3.
    
    didn't make the splash look better.

--- generatesplash.sh
+++ generatesplash.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-echo "Checking for magick..."
-if command -v magick >/dev/null 2>&1
+echo "Checking for rsvg..."
+if command -v rsvg-convert >/dev/null 2>&1
 then
-	echo "found magick."
+	echo "found rsvg-convert."
 else
-	echo "no magick found. skipping generation of splash" >&2
+	echo "no rsvg-convert found. skipping generation of splash" >&2
 	cp src/main/resources/de/thomas_oster/visicut/gui/resources/splash{-fallback,}.png
 	rm -f src/main/resources/de/thomas_oster/visicut/gui/resources/splash@{2,3}x.png
 	exit
@@ -15,10 +15,10 @@ echo "Version is: \"$VERSION\" (override with VERSION environment variable)"
 echo "Generating SVG"
 cat splashsource.svg|sed s#insert#$VERSION#g# > splash.svg
 echo "Converting to png"
-magick -background none splash.svg -resize 514x444 src/main/resources/de/thomas_oster/visicut/gui/resources/splash.png
+rsvg-convert -w 514 -h 444 splash.svg > src/main/resources/de/thomas_oster/visicut/gui/resources/splash.png
 # high-dpi variants (see https://docs.oracle.com/javase/10/docs/api/java/awt/SplashScreen.html )
-magick -background none splash.svg -resize 1028x888 src/main/resources/de/thomas_oster/visicut/gui/resources/splash@2x.png
-magick -background none splash.svg -resize 1542x1332 src/main/resources/de/thomas_oster/visicut/gui/resources/splash@3x.png
+rsvg-convert -w 1028 -h 888 splash.svg > src/main/resources/de/thomas_oster/visicut/gui/resources/splash@2x.png
+rsvg-convert -w 1542 -h 1332 splash.svg > src/main/resources/de/thomas_oster/visicut/gui/resources/splash@3x.png
 echo "cleaning..."
 rm splash.svg
 echo "done."
